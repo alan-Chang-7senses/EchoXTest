37daeefb14a5e061c8b24b4283bea0965a248e1a
連志偉 <zhiwei.lian@7senses.com>
2022-01-05 17:03:37 +0800

    DNA 取得角色部位碼規則移到 PlayerUtility。

---
+++ src/classes/Games/Utilities/PlayerUtility.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Games\Utilities;
+
+use Games\Consts\NFTDNA;
+/**
+ * Description of PlayerUtility
+ *
+ * @author Lian Zhi Wei <zhiwei.lian@7senses.com>
+ */
+class PlayerUtility {
+    
+    public static function PartCodeByDNA(string $dna) : string{
+        return substr($dna, NFTDNA::PartStart, NFTDNA::PartLength);
+    }
+}
