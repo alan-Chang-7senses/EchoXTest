40daa07c2e1ac1503845bd3338244f9ce1d38646
連志偉 <zhiwei.lian@7senses.com>
2022-01-10 18:50:20 +0800

    更新 API 程式碼：/MainMenu/CharacterSelectData/ - 移除 Avatar Class。 - API 資料改從資料存取層取得。

--- src/classes/Games/Accessors/PlayerAccessor.php
+++ src/classes/Games/Accessors/PlayerAccessor.php
@@ -9,6 +9,12 @@ namespace Games\Accessors;
  */
 class PlayerAccessor extends BaseAccessor{
     
+    public function countHolderByUserID(int $id) : int{
+        $row = $this->MainAccessor()->SelectExpr('COUNT(*) AS cnt')->FromTable('PlayerHolder')->WhereEqual('UserID', $id)->Fetch();
+        if(empty($row)) return 0;
+        return $row->cnt;
+    }
+
     public function rowPlayerJoinHolderByUserID(int $id) : mixed{
         return $this->MainAccessor()->FromTableJoinUsing('PlayerNFT', 'PlayerHolder', 'INNER', 'PlayerID')
                 ->WhereEqual('UserID', $id)->Fetch();
@@ -19,7 +25,7 @@ class PlayerAccessor extends BaseAccessor{
                 ->WhereEqual('PlayerID', $playerID)->WhereEqual('UserID', $userID)
                 ->Fetch();
     }
-
+    
     /**
      * 透過角色ID取得包含持有者與角色等級的單筆資料
      * @param int $id
@@ -41,5 +47,10 @@ class PlayerAccessor extends BaseAccessor{
         return $this->MainAccessor()->FromTable('PlayerSkill')
                 ->WhereEqual('PlayerID', $playerID)
                 ->WhereIn('SkillID', $skillIDs)->FetchAll();
+    }
+    
+    public function rowsPlayerJoinHolderByUserIDLimit(int $userID, int $offset = 0, int $count = 1) : array{
+        return $this->MainAccessor()->FromTableJoinUsing('PlayerNFT', 'PlayerHolder', 'INNER', 'PlayerID')
+                ->WhereEqual('UserID', $userID)->OrderBy('PlayerID')->Limit($count, $offset)->FetchAll();
     }
 }
