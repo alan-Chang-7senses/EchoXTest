e38fc6c70f505f2c35ffb6ad033b6fb333884992
連志偉 <zhiwei.lian@7senses.com>
2022-03-01 12:55:58 +0800

    修正不需登入之功能在紀錄 Log 時會因無 UserID 而發生錯誤的問題。

--- src/classes/Games/Accessors/GameLogAccessor.php
+++ src/classes/Games/Accessors/GameLogAccessor.php
@@ -14,7 +14,7 @@ class GameLogAccessor extends BaseAccessor{
     public function AddBaseProcess() : void{
         
         $this->LogAccessor()->FromTable('BaseProcess')->Add([
-            'UserID' => $_SESSION[Sessions::UserID],
+            'UserID' => $_SESSION[Sessions::UserID] ?? 0,
             'RedirectURL' => $GLOBALS[Globals::REDIRECT_URL],
             'Content' => json_encode($_POST, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES),
             'Result' => (int)$GLOBALS[Globals::RESULT_PROCESS],
