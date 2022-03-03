0453a5b4039ee0b3ee7b3ed451ab90708e3874ad
連志偉 <zhiwei.lian@7senses.com>
2022-03-03 16:47:35 +0800

    更新 API /Player/Skills/： - 整理程式碼，改為使用陣列建置回傳資料。 - 技能效果與滿星技能效果資料改為直接從技能處理器 SkillHandler 取得。

--- src/classes/Processors/Player/Skills.php
+++ src/classes/Processors/Player/Skills.php
@@ -4,13 +4,10 @@ namespace Processors\Player;
 
 use Consts\ErrorCode;
 use Games\Players\PlayerHandler;
-use Games\Skills\SkillEffectHandler;
 use Games\Skills\SkillHandler;
-use Games\Skills\SkillMaxEffectHandler;
 use Helpers\InputHelper;
 use Holders\ResultData;
 use Processors\BaseProcessor;
-use stdClass;
 /**
  * Description of Skills
  *
@@ -28,47 +25,24 @@ class Skills extends BaseProcessor{
         $skills = [];
         foreach ($playerInfo->skills as $playerSkill){
             
-            $skillInfo = (new SkillHandler($playerSkill->id))->GetInfo();
+            $handler = new SkillHandler($playerSkill->id);
+            $skillInfo = $handler->GetInfo();
             
-            $skill = new stdClass();
-            $skill->id = $skillInfo->id;
-            $skill->name = $skillInfo->name;
-            $skill->description = $skillInfo->description;
-            $skill->type = $skillInfo->type;
-            $skill->level = $playerSkill->level;
-            $skill->slot = $playerSkill->slot;
-            $skill->energy = $skillInfo->energy;
-            $skill->cooldown = $skillInfo->cooldown;
-            $skill->maxDescription = $skillInfo->maxDescription;
-            $skill->maxCondition = $skillInfo->maxCondition;
-            $skill->maxConditionValue = $skillInfo->maxConditionValue;
-            
-            $skill->effects = [];
-            foreach($skillInfo->effects as $skillEffectID){
-                
-                $skillEffectInfo = (new SkillEffectHandler($skillEffectID))->GetInfo();
-                
-                $skill->effects[] = [
-                    'type' => $skillEffectInfo->type,
-                    'target' => $skillEffectInfo->target,
-                    'duration' => $skillEffectInfo->duration,
-                    'formulaValue' => 0, //未計算
-                ];
-            }
-            
-            $skill->maxEffects = [];
-            foreach($skillInfo->maxEffects as $maxEffectID){
-                
-                $maxEffectInfo = (new SkillMaxEffectHandler($maxEffectID))->GetInfo();
-                
-                $skill->maxEffects[] = [
-                    'type' => $maxEffectInfo->type,
-                    'typeValue' => $maxEffectInfo->typeValue,
-                    'formulaValue' => 0, //未計算
-                ];
-            }
-            
-            $skills[] = $skill;
+            $skills[] = [
+                'id' => $skillInfo->id,
+                'name' => $skillInfo->name,
+                'description' => $skillInfo->description,
+                'type' => $skillInfo->type,
+                'level' => $playerSkill->level,
+                'slot' => $playerSkill->slot,
+                'energy' => $skillInfo->energy,
+                'cooldown' => $skillInfo->cooldown,
+                'maxDescription' => $skillInfo->maxDescription,
+                'maxCondition' => $skillInfo->maxCondition,
+                'maxConditionValue' => $skillInfo->maxConditionValue,
+                'effects' => $handler->GetEffects(),
+                'maxEffects' => $handler->GetMaxEffects(),
+            ];
         }
         
         $result = new ResultData(ErrorCode::Success);
