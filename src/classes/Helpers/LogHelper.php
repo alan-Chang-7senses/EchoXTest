7a6addb5497b33b8eecf7e197b9f0296de5468b5
連志偉 <zhiwei.lian@7senses.com>
2022-03-18 14:54:48 +0800

    更新系統log機制，辨識環境為 local 時才紀錄到檔案。

--- src/classes/Helpers/LogHelper.php
+++ src/classes/Helpers/LogHelper.php
@@ -2,6 +2,7 @@
 
 namespace Helpers;
 
+use Consts\EnvVar;
 use Consts\Folders;
 use Consts\Globals;
 use Consts\Predefined;
@@ -21,9 +22,16 @@ class LogHelper {
         
         if(empty(self::$root)) self::$root = PathHelper::getPath (Folders::Log);
         
+        $isLocal = getenv(EnvVar::SysEnv) == Predefined::SysLocal;
+        
         $log = new LogError();
-        $log->datetime = date(Predefined::FormatDatetime);
-        $log->timezone = date_default_timezone_get();
+        
+        if($isLocal){
+            
+            $log->datetime = date(Predefined::FormatDatetime);
+            $log->timezone = date_default_timezone_get();
+        }
+        
         $log->code = $ex->getCode();
         $log->file = $ex->getFile();
         $log->line = $ex->getLine();
@@ -36,9 +44,8 @@ class LogHelper {
         $log->processTime = microtime(true) - $GLOBALS[Globals::TIME_BEGIN];
         self::$extra = null;
         
-        error_log(json_encode($log, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT).PHP_EOL.PHP_EOL, 3, self::$root.date('Ymd').'.log');
+        if($isLocal) error_log(json_encode($log, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT).PHP_EOL.PHP_EOL, 3, self::$root.date('Ymd').'.log');
         
-        unset($log->datetime);
         error_log(json_encode($log, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES), 0);
         
         $GLOBALS[Globals::RESULT_PROCESS_MESSAGE] = $log->message;
