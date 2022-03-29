161f054f5634375032090cca4def6b52b06e37c3
連志偉 <zhiwei.lian@7senses.com>
2022-03-29 16:05:04 +0800

    新增 CLI 執行機制： - 新增公用的 autoload。 - 遊戲專用 CLI 入口。 - CLI 指令新增 CreatePlayerSkill。

---
+++ src/classes/Games/CommandWorkers/CreatePlayerSkill.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Games\CommandWorkers;
+
+use Games\Accessors\SkillAccessor;
+use Games\Accessors\ToolAccessor;
+use Games\Consts\PlayerValue;
+use Games\Players\PlayerUtility;
+/**
+ * Description of CreatePlayerSkill
+ *
+ * @author Lian Zhi Wei <zhiwei.lian@7senses.com>
+ */
+class CreatePlayerSkill extends BaseWorker{
+    
+    public function Process(): array {
+        
+        $accessor = new ToolAccessor();
+        
+        $rowsOld = $accessor->rowsPlayerSkill();
+        
+        $rowsNFT = $accessor->rowsPlayerNFT();
+        $accessorSkill = new SkillAccessor();
+        $playerSkillAlias = [];
+        $allAliasCodes = [];
+        foreach($rowsNFT as $row){
+            
+            $aliasCodes = [];
+            $aliasCodes = array_merge($aliasCodes, $accessorSkill->aliasCodesByPart(PlayerUtility::PartCodeByDNA($row->HeadDNA), PlayerValue::Head));
+            $aliasCodes = array_merge($aliasCodes, $accessorSkill->aliasCodesByPart(PlayerUtility::PartCodeByDNA($row->BodyDNA), PlayerValue::Body));
+            $aliasCodes = array_merge($aliasCodes, $accessorSkill->aliasCodesByPart(PlayerUtility::PartCodeByDNA($row->HandDNA), PlayerValue::Hand));
+            $aliasCodes = array_merge($aliasCodes, $accessorSkill->aliasCodesByPart(PlayerUtility::PartCodeByDNA($row->LegDNA), PlayerValue::Leg));
+            $aliasCodes = array_merge($aliasCodes, $accessorSkill->aliasCodesByPart(PlayerUtility::PartCodeByDNA($row->BackDNA), PlayerValue::Back));
+            $aliasCodes = array_merge($aliasCodes, $accessorSkill->aliasCodesByPart(PlayerUtility::PartCodeByDNA($row->HatDNA), PlayerValue::Hat));
+            $aliasCodes = array_filter(array_unique($aliasCodes));
+            
+            $playerSkillAlias[$row->PlayerID] = $aliasCodes;
+            
+            $allAliasCodes = array_merge($allAliasCodes, $aliasCodes);
+        }
+        
+        $allAliasCodes = array_values(array_unique($allAliasCodes));
+        $rowsSkillInfo = $accessorSkill->rowsInfoByAliasCodes($allAliasCodes, true);
+        $skillIDs = array_combine(array_column($rowsSkillInfo, 'AliasCode'), array_column($rowsSkillInfo, 'SkillID'));
+        
+        $addData = [];
+        foreach($playerSkillAlias as $playerID => $aliasCodes){
+            foreach($aliasCodes as $aliasCode) $addData[] = ['PlayerID' => $playerID, 'SkillId' => $skillIDs[$aliasCode]];
+        }
+        
+        $res['clear'] = $accessor->ClearPlayerSkill();
+        $res['add'] = $accessor->AddPlayerSkills($addData);
+        foreach($rowsOld as $row) $res['modify'][$row->PlayerID][$row->SkillID] = $accessor->ModifyPlayerSkillByPlayerAndSkill($row->PlayerID, $row->SkillID, ['Level' => $row->Level, 'Slot' => $row->Slot]);
+        
+        return $res;
+    }
+}
---
+++ src/cli/Games.php
@@ -0,0 +1,30 @@
+<?php
+
+if($argc <= 1) exit('No argv, must use command format: php Games.php commandName key1=value1 key2=value2 ....'.PHP_EOL.PHP_EOL);
+
+require str_replace('/', DIRECTORY_SEPARATOR, __DIR__. '/../autoload.php');
+
+$class = 'Games\\CommandWorkers\\'.$argv[1];
+
+
+try{
+    
+    $reflectionClass = new ReflectionClass($class);
+    if(!$reflectionClass->isInstantiable()) throw new ReflectionException ('Class '.$class.' does not instantiable.');
+
+    $obj = new $class($argc, $argv);
+    $result = $obj->Process();
+    
+} catch (ReflectionException $ex){
+    
+    $result = [
+        'code' => $ex->getCode(),
+        'file' => $ex->getFile(),
+        'line' => $ex->getLine(),
+        'messqge' => $ex->getMessage(),
+        'trace' => $ex->getTrace(),
+    ];
+    
+}
+
+echo json_encode ($result, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT).PHP_EOL.PHP_EOL;
