942b0fb46f8c2d259985f0211f18ab6918b3b06b
連志偉 <zhiwei.lian@7senses.com>
2022-05-10 18:50:16 +0800

    新增 API /Races/BotPlayer/，取得用於配對的機器人角色資料。

---
+++ src/classes/Processors/Races/BotPlayer.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Processors\Races;
+
+use Consts\ErrorCode;
+use Games\Accessors\UserAccessor;
+use Games\Consts\RaceValue;
+use Games\Players\PlayerHandler;
+use Games\Players\PlayerUtility;
+use Games\Users\UserHandler;
+use Helpers\InputHelper;
+use Holders\ResultData;
+use Processors\Races\BaseRace;
+/**
+ * Description of botPlayer
+ *
+ * @author Lian Zhi Wei <zhiwei.lian@7senses.com>
+ */
+class BotPlayer extends BaseRace{
+    
+    protected bool|null $mustInRace = false;
+
+    public function Process(): ResultData {
+        
+        $amount = InputHelper::post('amount');
+        if(empty($amount)) $amount = 1;
+        
+        $rows = (new UserAccessor())->rowsByIdleBotAssoc($amount);
+        $playerIDs = array_column($rows, 'Player');
+        $userIDs = array_column($rows, 'UserID');
+        
+        $players = [];
+        foreach($playerIDs as $id){
+            
+            $hanlder = new PlayerHandler($id);
+            $info = $hanlder->GetInfo();
+            
+            $players[] = [
+                'id' => $info->id,
+                'name' => $info->name,
+                'head' => PlayerUtility::PartCodeByDNA($info->dna->head),
+                'body' => PlayerUtility::PartCodeByDNA($info->dna->body),
+                'hand' => PlayerUtility::PartCodeByDNA($info->dna->hand),
+                'leg' => PlayerUtility::PartCodeByDNA($info->dna->leg),
+                'back' => PlayerUtility::PartCodeByDNA($info->dna->back),
+                'hat' => PlayerUtility::PartCodeByDNA($info->dna->hat),
+            ];
+        }
+        
+        foreach($userIDs as $id){
+            
+            $handler = new UserHandler($id);
+            $handler->SaveData(['Race' => RaceValue::BotMatch]);
+        }
+        
+        $result = new ResultData(ErrorCode::Success);
+        $result->players = $players;
+        return $result;
+    }
+
+}
