f5ba56723dfcc138d8dd82a675fddbfa6824868e
連志偉 <zhiwei.lian@7senses.com>
2022-05-19 16:04:30 +0800

    新增 API /Notices/Marquee/，撈取跑馬燈資料。。

---
+++ src/classes/Games/Consts/NoticeValue.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Games\Consts;
+
+/**
+ * Description of NoticeValue
+ *
+ * @author Lian Zhi Wei <zhiwei.lian@7senses.com>
+ */
+class NoticeValue {
+    
+    const StatusDisabled = 0;
+    const StatusEnabled = 1;
+    
+    const LanguageEnglish = 1;
+    const TraditionalChinese = 2;
+    const SimplifiedChinese = 3;
+}
