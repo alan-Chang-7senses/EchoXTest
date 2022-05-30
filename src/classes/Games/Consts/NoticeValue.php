0bb3f5a1497502e2d8ad1e1e914323580a4b3597
連志偉 <zhiwei.lian@7senses.com>
2022-05-30 17:16:32 +0800

    定義語言代號，並更新 API 文件。

--- src/classes/Games/Consts/NoticeValue.php
+++ src/classes/Games/Consts/NoticeValue.php
@@ -12,7 +12,17 @@ class NoticeValue {
     const StatusDisabled = 0;
     const StatusEnabled = 1;
     
-    const LanguageEnglish = 1;
-    const TraditionalChinese = 2;
-    const SimplifiedChinese = 3;
+    const LangDefault = 0;
+    const LangGerman = 1;
+    const LangEnglish = 2;
+    const LangSpanish = 3;
+    const LangFilipino = 4;
+    const LangIndonesian = 5;
+    const LangJapanese = 6;
+    const LangKorean = 7;
+    const LangBurmese = 8;
+    const LangPortuguese = 9;
+    const LangRussian = 10;
+    const LangThai = 11;
+    const LangTraditionalChinese = 12;
 }
