aee0439edaad71fc14cba9d7e77ec24cce7745f9
連志偉 <zhiwei.lian@7senses.com>
2022-08-19 14:06:01 +0800

    新增 API /Leaderboard/CB15/RaceFastest/， CB 1.5 最快排行榜。

---
+++ src/classes/Processors/Leaderboard/BaseLeaderboard.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Processors\Leaderboard;
+
+use Processors\BaseProcessor;
+/**
+ * Description of BaseLeaderboard
+ *
+ * @author Lian Zhi Wei <zhiwei.lian@7senses.com>
+ */
+abstract class BaseLeaderboard extends BaseProcessor{
+    
+    protected bool $mustSigned = false;
+    protected bool $maintainMode = false;
+    
+    protected int $offset;
+    protected int $length;
+    
+    const DefaultLength = 12;
+    public function __construct() {
+        parent::__construct();
+        
+        $page = filter_input(INPUT_GET, 'page');
+        if(empty($page) || $page <= 0) $page = 1;
+        
+        $length = filter_input(INPUT_GET, 'length');
+        if(empty($length)) $length = self::DefaultLength;
+        
+        $this->offset = ($page - 1) * $length;
+        $this->length = $length;
+    }
+}
---
+++ src/classes/Processors/Leaderboard/CB15/RaceFastest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Processors\Leaderboard\CB15;
+
+use Accessors\PDOAccessor;
+use Consts\EnvVar;
+use Consts\ErrorCode;
+use Holders\ResultData;
+use Processors\Leaderboard\BaseLeaderboard;
+/**
+ * Description of RaceFastest
+ *
+ * @author Lian Zhi Wei <zhiwei.lian@7senses.com>
+ */
+class RaceFastest extends BaseLeaderboard{
+    
+    public function Process(): ResultData {
+        
+        $accessor = new PDOAccessor(EnvVar::DBMain);
+        $rows = $accessor->SelectExpr('*, SUBSTRING( FROM_UNIXTIME(`Fastest`, "%T.%f"), 1, 12) AS Duration')->FromTableJoinUsing('UserRaceTiming', 'Users', 'LEFT', 'UserID')
+                ->Limit($this->length, $this->offset)->OrderBy('Fastest')->FetchAll();
+                //FromTable('UserRaceTiming')->Limit($this->length, $this->offset)->OrderBy('Fastest')->FetchAll();
+        
+        $list = [];
+        $ranking = $this->offset + 1;
+        foreach($rows as $row){
+            
+            $list[] = [
+                'ranking' => $ranking,
+                'nickname' => $row->Nickname,
+                'duration' => $row->Duration,
+            ];
+            
+            ++$ranking;
+        }
+        
+        $result = new ResultData(ErrorCode::Success);
+        $result->list = $list;
+        return $result;
+    }
+}
