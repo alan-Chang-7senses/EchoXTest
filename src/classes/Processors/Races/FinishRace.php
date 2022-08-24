f7079327f4d5c18682c4ba9ec4cb558bfde34c9c
連志偉 <zhiwei.lian@7senses.com>
2022-08-24 11:36:24 +0800

    修改 /Races/FinishRace/，移除輸入參數，以後端資料為主來進行完賽流程。

--- src/classes/Processors/Races/FinishRace.php
+++ src/classes/Processors/Races/FinishRace.php
@@ -18,9 +18,6 @@ use Games\Races\RaceUtility;
 use Games\Users\RewardHandler;
 use Games\Users\UserBagHandler;
 use Games\Users\UserUtility;
-use Generators\ConfigGenerator;
-use Generators\DataGenerator;
-use Helpers\InputHelper;
 use Holders\ResultData;
 /**
  * Description of FinishRace
@@ -37,20 +34,12 @@ class FinishRace extends BaseRace{
 
     public function Process(): ResultData {
         
-        $players = json_decode(InputHelper::post('players'));
-        $playerCount = count($players);
-        if(!is_array($players) || $playerCount <= 0 || $playerCount > ConfigGenerator::Instance()->AmountRacePlayerMax) throw new RaceException(RaceException::IncorrectPlayerNumber);
-        DataGenerator::ExistProperties($players[0], ['id', 'ranking']);
-        
-        $rankings = [];
-        foreach($players as $player){
-            $rankings[$player->id] = $player->ranking;
-        }
-        
         $raceID = $this->userInfo->race;
         $racePool = RacePool::Instance();
         $raceInfo = $racePool->$raceID;
         if($raceInfo->status == RaceValue::StatusFinish) throw new RaceException(RaceException::Finished);
+        
+        $playerCount = count((array)$raceInfo->racePlayers);
         
         $userPool = UserPool::Instance();
         $racePlayerPool = RacePlayerPool::Instance();
@@ -62,14 +51,6 @@ class FinishRace extends BaseRace{
             
             $racePlayerInfo = $racePlayerPool->$racePlayerID;
             if($racePlayerInfo->status != RaceValue::StatusReach) throw new RaceException(RaceException::PlayerNotReached, ['[player]' => $racePlayerInfo->player]);
-            
-            if(!isset($rankings[$racePlayerInfo->player]) || $rankings[$racePlayerInfo->player] != $racePlayerInfo->ranking){
-                throw new RaceException(RaceException::RankingNoMatch, [
-                    '[player]' => $racePlayerInfo->player,
-                    '[front]' => $rankings[$racePlayerInfo->player] ?? 0,
-                    '[back]' => $racePlayerInfo->ranking,
-                ]);
-            }
             
             $rewardID = $singleRankingRewardPool->GetInfo($playerCount, $racePlayerInfo->ranking)->{$this->rewardField[$this->userInfo->lobby]};
             $rewardHandler = new RewardHandler($rewardID);
