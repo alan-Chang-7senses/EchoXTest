2168dc012428decb164c1fc4ece7477f4ecff264
gene <gene@7senses.com>
2022-08-26 11:55:31 +0800

    新增HintTextPool

---
+++ src/classes/Games/Pools/HintTextPool.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Games\Pools;
+
+use Accessors\PoolAccessor;
+
+/**
+ * Description of HintTextPool
+ */
+class HintTextPool extends PoolAccessor {
+
+    public function FromDB(int|string $id): stdClass|false {
+        
+    }
+
+}
