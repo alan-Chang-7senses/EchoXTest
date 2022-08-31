2b51cc1c55eb242087f70dfd656fe71f895a50e9
連志偉 <zhiwei.lian@7senses.com>
2022-08-31 21:48:59 +0800

    修正資料池暫存錯誤問題。

--- src/classes/Accessors/PoolAccessor.php
+++ src/classes/Accessors/PoolAccessor.php
@@ -27,39 +27,39 @@ abstract class PoolAccessor {
             $data = $this->FromDB($id);
             $mem->set($key, json_encode($data));
         }
-//        $mem->delete($key);
-        $this->$key = $data;
+        
+        $this->$id = $data;
         return $data;
     }
     
     public function Set(string|int $id, string $property, mixed $value){
         
         $key = $this->keyPrefix.$id;
-        $data = $this->$key;
+        $data = $this->$id;
         
         $data->$property = $value;
         MemcacheAccessor::Instance()->set($key, json_encode($data));
         
-        $this->$key = $data;
+        $this->$id = $data;
     }
     
     public function Save(string|int $id, string $property, mixed $value){
         
         $key = $this->keyPrefix.$id;
-        $data = $this->$key;
+        $data = $this->$id;
         
         $method = 'Save'.ucfirst($property);
         if(method_exists($this, $method)) $data = $this->$method($data, $value);
         else throw new Exception(get_called_class ().' no method '.$method, ErrorCode::SystemError);
         
         MemcacheAccessor::Instance()->set($key, json_encode($data));
-        $this->$key = $data;
+        $this->$id = $data;
     }
     
     public function Delete(string $id) : bool{
         $key = $this->keyPrefix.$id;
         $res = MemcacheAccessor::Instance()->delete($key);
-        if($res) unset ($this->$key);
+        if($res) unset ($this->$id);
         return $res;
     }
 }
