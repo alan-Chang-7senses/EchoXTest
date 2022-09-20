ebdb3de2c69315bba31ef655accf3ecc674c723e
alan-Chang-7senses <108779046+alan-Chang-7senses@users.noreply.github.com>
2022-09-20 15:18:11 +0800

    將取得免費player功能分離

--- src/classes/Games/FreePlayer/FreePlayerHandler.php
+++ src/classes/Games/FreePlayer/FreePlayerHandler.php
@@ -5,6 +5,7 @@ namespace Games\FreePlayer;
 use Games\Players\Holders\PlayerInfoHolder;
 use Games\Players\PlayerHandler;
 use Games\Pools\FreePlayerPool;
+use Games\Skills\SkillHandler;
 use stdClass;
 
 class FreePlayerHandler extends PlayerHandler
@@ -26,4 +27,45 @@ class FreePlayerHandler extends PlayerHandler
     {
         return $this->info;
     }
+
+    public function HandleFreePlayerData() : FreePlayerInfo
+    {
+        $rt = new FreePlayerInfo();
+        $rt->number = $this->info->ele;
+        $rt->type = $this->info->type;
+        $rt->ele = $this->info->ele;
+        $rt->velocity = $this->info->velocity;
+        $rt->stamina = $this->info->stamina;
+        $rt->breakOut = $this->info->breakOut;
+        $rt->will = $this->info->will;
+        $rt->intelligent = $this->info->intelligent;
+        $rt->dna = $this->info->dna;
+        $rt->baseInfo = $this->info->freePlayerBase;
+        foreach($this->info->skills as $skill)
+        {
+            $handler = new SkillHandler($skill->id);
+            $handler->playerHandler = $this;
+            $skillInfo = $handler->GetInfo();                
+            $rt->skills[] = 
+            [
+                "id" => $skillInfo->id,
+                "name" => $skillInfo->name,
+                "icon" => $skillInfo->icon,
+                "description" => $skillInfo->description,
+                "energy" => $skillInfo->energy,
+                "cooldowm" => $skillInfo->cooldown,
+                "duration" => $skillInfo->duration,
+                "ranks" => $skillInfo->ranks,
+                "maxDescription" => $skillInfo->maxDescription,
+                "maxCondition" => $skillInfo->maxCondition,
+                "maxConditionValue" => $skillInfo->maxConditionValue,
+                "effects" => $handler->GetEffects(),
+                "maxEffects" => $handler->GetMaxEffects(),
+            ];
+            
+        }
+        return $rt;
+    }
+
+
 }
--- src/classes/Games/Users/FreePeta/FreePetaUtility.php
+++ src/classes/Games/Users/FreePeta/FreePetaUtility.php
@@ -1,12 +1,18 @@
 <?php
 namespace Games\Users\FreePeta;
 
+use Accessors\PDOAccessor;
+use Consts\EnvVar;
+use Consts\Sessions;
 use Games\Consts\FreePlayerValue;
 use Games\Consts\PlayerValue;
+use Games\Exceptions\UserException;
 use Games\FreePlayer\FreePlayerHandler;
+use Games\FreePlayer\FreePlayerInfo;
 use Games\Players\Holders\PlayerDnaHolder;
 use Games\Players\Holders\PlayerInfoHolder;
 use Games\Players\PlayerUtility;
+use Games\Pools\FreePlayerPool;
 use Games\Random\RandomUtility;
 use Games\Users\UserHandler;
 use stdClass;
@@ -51,13 +57,27 @@ class FreePetaUtility
      /**
      * @param ?int $type 可指定獲取免費角色種類(速度型：1、平衡型：2、持久型：3)，未填入則隨機給予。
      */
-    public static function GetFreePlayer(int $userID,?int $type = null) : PlayerInfoHolder|stdClass
+    public static function GetFreePlayer(?int $type = null) : FreePlayerInfo
     {
         $type = $type ?? RandomUtility::GetRandomObject(FreePlayerValue::FreePlayerTypeSpeed,FreePlayerValue::FreePlayerTypeBalance,FreePlayerValue::FreePlayerTypeLasting);
+        
+        $info = (new FreePlayerHandler($type))->HandleFreePlayerData();
+        FreePlayerPool::Instance()->Delete($type);
+        return $info;
+    }
+    public static function AddNewFreePlayer(FreePlayerInfo|stdClass $freePlayer,int $userID)
+    {
+        //PlayerNFT, PlayerHolder, PlayerLevel, PlayerSkill
         $count = self::GetUserFreePlayerCount($userID);
         $id = $userID * PlayerValue::freePetaPlayerIDMultiplier + $count + 1;
-        $info = (new FreePlayerHandler($type))->GetInfo();
-        $info->id = $id;
-        return $info;
+
+        if($id >= ($userID + 1) * PlayerValue::freePetaPlayerIDMultiplier)
+        throw new UserException(UserException::UserFreePlayerOverLimit,['userID' => $_SESSION[Sessions::UserID]]);
+
+        $accessor = new PDOAccessor(EnvVar::DBMain);
+        $accessor->FromTable('PlayerNFT')->Add(
+            [
+
+            ]);
     }
 }
