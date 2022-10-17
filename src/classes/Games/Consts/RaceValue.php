e3b8309fe7d071221a71146e2cef4d57ac0ec21b
alan-Chang-7senses <108779046+alan-Chang-7senses@users.noreply.github.com>
2022-10-17 16:44:16 +0800

    更改S值體力充沛前後係數

--- src/classes/Games/Consts/RaceValue.php
+++ src/classes/Games/Consts/RaceValue.php
@@ -89,24 +89,25 @@ class RaceValue {
     const RetainStrength = 3;
 
     /**直線x體力充沛 前係數 */
-    const PositiveHPStraightFront = 0.75;
+    const PositiveHPStraightFront = 0.55;
     /**直線x體力充沛 後係數 */
-    const PositiveHPStraightBack = 0.25;
+    const PositiveHPStraightBack = 0.33;
     /**直線x體力耗盡 前係數 */
     const MinusHPStraightFront = 0.25;
     /**直線x體力耗盡 後係數 */
     const MinusHPStraightBack = 0.25;
     /**彎道x體力充沛 前係數 */
-    const PositiveHPCurveFront = 0.75;
+    const PositiveHPCurveFront = 0.55;
     /**彎道x體力充沛 後係數 */
-    const PositiveHPCurveBack = 0.25;
+    const PositiveHPCurveBack = 0.33;
     /**彎道x體力耗盡 前係數 */
     const MinusHPCurveFront = 0.25;
     /**彎道x體力耗盡 後係數 */
     const MinusHPCurveBack = 0.25;
 
     /**體力充沛S基數 */
-    const SValueBasePositiveHP = 0;
+    const SValueBasePositiveHP = 2.5;
+    /**體力耗盡S基數 */
     const SValueBaseMinusHP = 0;
 
     const LobbyNone = 0;
