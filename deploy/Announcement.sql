65ada1fae063330173791cd352e064afcd45485c
alan-Chang-7senses <108779046+alan-Chang-7senses@users.noreply.github.com>
2022-10-24 18:15:41 +0800

    修改資料表內容

--- deploy/Announcement.sql
+++ deploy/Announcement.sql
@@ -1,4 +1,3 @@
-
 USE `koa_static`;
 
 -- 傾印  資料表 koa_static.Announcement 結構
@@ -13,8 +12,12 @@ CREATE TABLE IF NOT EXISTS `Announcement` (
   `CreateTime` varchar(50) NOT NULL DEFAULT '' COMMENT '創建時間。非發佈時間',
   `PublishTime` int(11) NOT NULL DEFAULT 0 COMMENT '發佈時間',
   `FinishTime` int(11) NOT NULL DEFAULT 0 COMMENT '結束時間',
-  PRIMARY KEY (`Serial`,`ID`),
+  PRIMARY KEY (`Serial`) USING BTREE,
   KEY `Lang` (`Lang`),
-  KEY `PublishTime` (`PublishTime`)
+  KEY `PublishTime` (`PublishTime`),
+  KEY `ID` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='公告看板資料集合';
+
+INSERT INTO `Announcement` (`Serial`, `ID`, `Type`, `Lang`, `GraphURL`, `Title`, `Content`, `CreateTime`, `PublishTime`, `FinishTime`) VALUES
+	(1, 1, 2, 12, '...', '我是標題', '我是公告\r\n我是公告\r\n我是公告', '0000-00-00', 1666597667, 2666607667);
 
