a005a9f3010c38e138a38a27c7165eddb6f3b146
連志偉 <zhiwei.lian@7senses.com>
2022-10-31 19:43:04 +0800

    更新 NFTFactory 處理 NFT 資料相關功能（未完成）。 - 定義 狀態常數、階段常數、屬性類型常數、DNA 長度與偏移常數、原生種數值。 - 定義 NFT ID 轉 PlayerID 的添加數值。 - 新增 Metadata dna 資料持有類，用於放置 Metadata DNA 的資料。 - 新增 部位技能持有類，用於暫存部位技能別名。 - NFTFactory 添加重整角色資料及技能等功能（未完成）。 - NFTUtility 添加功能處理 PlayerID、部位 DNA、部位技能等。

--- src/classes/Games/Consts/NFTQueryValue.php
+++ src/classes/Games/Consts/NFTQueryValue.php
@@ -17,6 +17,31 @@ class NFTQueryValue {
     const StatusFailure = 1;
     const StatusUnknow = 2;
     const StatusMaintenance = 3;
+    const StatusInvalidCredentials = 4;
     /** 無此信箱或信箱未綁定錢包 - 將查無 NFTs */
-    const StatusEmailNotFound = 4;
+    const StatusEmailNotFound = 5;
+    
+    const StageEgg = 'EGG';
+    const StageAdult = 'ADULT';
+    
+    const AttributesClass = [
+        'FIRE' => PlayerAttr::Fire,
+        'WATER' => PlayerAttr::Water,
+        'WOOD' => PlayerAttr::Wood,
+    ];
+    
+    const DNAPartHeadOffset = 0;
+    const DNAPartHeadLength = 10;
+    const DNAPartBodyOffset = 10;
+    const DNAPartBodyLength = 10;
+    const DNAPartHandOffset = 20;
+    const DNAPartHandLength = 10;
+    const DNAPartLegOffset = 30;
+    const DNAPartLegLength = 10;
+    const DNAPartBackOffset = 40;
+    const DNAPartBackLength = 10;
+    const DNAPartHatOffset = 50;
+    const DNAPartHatLength = 10;
+    
+    const NativeValue = 'YES';
 }
--- src/classes/Games/NFTs/NFTFactory.php
+++ src/classes/Games/NFTs/NFTFactory.php
@@ -10,6 +10,11 @@ use Games\Exceptions\NFTException;
 use Games\NFTs\Holders\NFTUserHolder;
 use Helpers\LogHelper;
 use stdClass;
+use Games\NFTs\Holders\MetadataDNAHolder;
+use Games\Players\PlayerUtility;
+use Games\NFTs\Holders\PartSkillHolder;
+use Games\Consts\PlayerValue;
+use Games\Players\Holders\PlayerDnaHolder;
 /**
  * Description of NFTFactory
  *
@@ -37,8 +42,11 @@ class NFTFactory {
         
         $status = NFTQueryValue::StatusSuccess;
         if($nfts === null) $status = NFTQueryValue::StatusFailure;
-        else if(isset($nfts->errors[0]) && $nfts->errors[0]->message == 'IN MAINTENANCE') $status = NFTQueryValue::StatusMaintenance;
-        else if(!empty($nfts->error)){
+        else if(isset($nfts->errors[0])){
+            if($nfts->errors[0]->message == 'IN MAINTENANCE') $status = NFTQueryValue::StatusMaintenance;
+            else if($nfts->errors[0]->message == 'invalidCredentials') $status = NFTQueryValue::StatusInvalidCredentials;
+            else $status = NFTQueryValue::StatusUnknow;
+        }else if(!empty($nfts->error)){
             if($nfts->error == 'emailNotFound') $status = NFTQueryValue::StatusEmailNotFound;
             else $status = NFTQueryValue::StatusUnknow;
         }
@@ -54,5 +62,147 @@ class NFTFactory {
         }
         
         return $nfts->result;
+    }
+    
+    private function QueryMetadata(string $metadataURL) : string|false{
+        
+        $curl = new CurlAccessor($metadataURL);
+        $curlReturn = $curl->ExecOption(CURLOPT_RETURNTRANSFER, true);
+        $metadata = json_decode($curlReturn);
+        return $metadata ?? false;
+    }
+    
+    private function PartSkill(PlayerDnaHolder $dnaHolder) : PartSkillHolder{
+        
+        $holder = new PartSkillHolder();
+        
+        $accessor = AccessorFactory::Static();
+        $accessor->PrepareName('NFTPartSkill')->FromTable('SkillPart');
+        
+        $row = $accessor->WhereEqual('PartCode', PlayerUtility::PartCodeByDNA($dnaHolder->head))->WhereEqual('PartType', PlayerValue::Head)->Fetch();
+        if($row !== false) $holder = NFTUtility::HoldPartSkill([$row->AliasCode1, $row->AliasCode2, $row->AliasCode3], $row->SkillAffixID, $holder);
+        
+        $row = $accessor->ClearCondition()->WhereEqual('PartCode', PlayerUtility::PartCodeByDNA($dnaHolder->body))->WhereEqual('PartType', PlayerValue::Body)->Fetch();
+        if($row !== false) $holder = NFTUtility::HoldPartSkill([$row->AliasCode1, $row->AliasCode2, $row->AliasCode3], $row->SkillAffixID, $holder);
+        
+        $row = $accessor->ClearCondition()->WhereEqual('PartCode', PlayerUtility::PartCodeByDNA($dnaHolder->hand))->WhereEqual('PartType', PlayerValue::Hand)->Fetch();
+        if($row !== false) $holder = NFTUtility::HoldPartSkill([$row->AliasCode1, $row->AliasCode2, $row->AliasCode3], $row->SkillAffixID, $holder);
+        
+        $row = $accessor->ClearCondition()->WhereEqual('PartCode', PlayerUtility::PartCodeByDNA($dnaHolder->leg))->WhereEqual('PartType', PlayerValue::Leg)->Fetch();
+        if($row !== false) $holder = NFTUtility::HoldPartSkill([$row->AliasCode1, $row->AliasCode2, $row->AliasCode3], $row->SkillAffixID, $holder);
+        
+        $row = $accessor->ClearCondition()->WhereEqual('PartCode', PlayerUtility::PartCodeByDNA($dnaHolder->back))->WhereEqual('PartType', PlayerValue::Back)->Fetch();
+        if($row !== false) $holder = NFTUtility::HoldPartSkill([$row->AliasCode1, $row->AliasCode2, $row->AliasCode3], $row->SkillAffixID, $holder);
+        
+        $row = $accessor->ClearCondition()->WhereEqual('PartCode', PlayerUtility::PartCodeByDNA($dnaHolder->hat))->WhereEqual('PartType', PlayerValue::Hat)->Fetch();
+        if($row !== false) $holder = NFTUtility::HoldPartSkill([$row->AliasCode1, $row->AliasCode2, $row->AliasCode3], $row->SkillAffixID, $holder);
+        
+        return $holder;
+    }
+    
+    private function AffixSkill(PartSkillHolder $holder) : array{
+        
+        $affixID = $holder->SortFirstAffixID();
+        
+        $accessor = AccessorFactory::Static();
+        $row = $accessor->PrepareName('NFTAffixSkill')->FromTable('SkillAffixAlias')->WhereEqual('SkillAffixID', $affixID)->Fetch();
+        if($row === false) return '';
+        
+        $level = $holder->totalSkillffixID[$affixID];
+        $aliasCodes[] = $row->{'Level'.$level};
+        
+        if($level == 3){
+            
+            $affixID = $holder->NextAffixID();
+            $row = $accessor->ClearCondition()->WhereEqual('SkillAffixID', $affixID)->Fetch();
+            if($row !== false && $holder->totalSkillffixID[$affixID] == 3) $aliasCodes[] = $row->Level3;
+        }
+        
+        return $aliasCodes;
+    }
+    
+    private function CreatePlayer($playerID, $metadata) : void{
+        
+        $playerHolder = [
+            'PlayerID' => $playerID,
+            'UserID' => $this->userHolder->userID,
+        ];
+        
+        $playerLevel = ['PlayerID' => $playerID];
+        
+        $metadataDNAHolder = new MetadataDNAHolder($metadata->properties->dna->dPart, $metadata->properties->dna->rPart1, $metadata->properties->dna->rPart2);
+        $playerDNAHolder = new PlayerDnaHolder();
+        $playerDNAHolder->head = NFTUtility::MetadataDNAToPartDNA($metadataDNAHolder, NFTQueryValue::DNAPartHeadOffset, NFTQueryValue::DNAPartHeadLength);
+        $playerDNAHolder->body = NFTUtility::MetadataDNAToPartDNA($metadataDNAHolder, NFTQueryValue::DNAPartBodyOffset, NFTQueryValue::DNAPartBodyLength);
+        $playerDNAHolder->hand = NFTUtility::MetadataDNAToPartDNA($metadataDNAHolder, NFTQueryValue::DNAPartHandOffset, NFTQueryValue::DNAPartHandLength);
+        $playerDNAHolder->leg = NFTUtility::MetadataDNAToPartDNA($metadataDNAHolder, NFTQueryValue::DNAPartLegOffset, NFTQueryValue::DNAPartLegLength);
+        $playerDNAHolder->back = NFTUtility::MetadataDNAToPartDNA($metadataDNAHolder, NFTQueryValue::DNAPartBackOffset, NFTQueryValue::DNAPartBackLength);
+        $playerDNAHolder->hat = NFTUtility::MetadataDNAToPartDNA($metadataDNAHolder, NFTQueryValue::DNAPartHatOffset, NFTQueryValue::DNAPartHatLength);
+        
+        $playerNFT = [
+            'PlayerID' => $playerID,
+            'TokenName' => $metadata->name,
+            'Constitution' => $metadata->attributes->attr_1,
+            'Strength' => $metadata->attributes->attr_2,
+            'Dexterity' => $metadata->attributes->attr_3,
+            'Agility' => $metadata->attributes->attr_4,
+            'Attribute' => NFTQueryValue::AttributesClass[$metadata->attributes->class],
+            'HeadDNA' => $playerDNAHolder->hand,
+            'BodyDNA' => $playerDNAHolder->body,
+            'HandDNA' => $playerDNAHolder->hand,
+            'LegDNA' => $playerDNAHolder->leg,
+            'BackDNA' => $playerDNAHolder->back,
+            'HatDNA' => $playerDNAHolder->hat,
+            
+            'StrengthLevel' => $metadata->properties->attribute_model,
+        ];
+        
+        $aliasCodes = [];
+        $partSkillHolder = $this->PartSkill($playerDNAHolder);
+        $aliasCodes = array_merge($aliasCodes, $partSkillHolder->aliasCodes);
+        
+        $affixSkillAliasCodes = $this->AffixSkill($partSkillHolder);
+        $aliasCodes = array_merge($aliasCodes, $affixSkillAliasCodes);
+        
+    }
+    
+    public function RefreshPlayers() : void{
+        
+        $nftsResult = $this->QueryNFTs($this->userHolder->email);
+        if($nftsResult === false) return;
+        
+        $accessor = AccessorFactory::Main();
+        
+        $metadataURLs = [];
+        $playerIDs = [];
+        foreach($nftsResult as $sympol => $nfts){
+            
+            if(!in_array($sympol, NFTQueryValue::ContractSymbols))
+                continue;
+            
+            foreach($nfts as $nft){
+                
+                if($nft->stage != NFTQueryValue::StageAdult)
+                    continue;
+                
+                $playerID = NFTUtility::ToPlayerID($nft->nftId);
+                $playerIDs[] = $playerID;
+                $metadataURLs[$playerID] = $nft->metadataUrl;
+            }
+        }
+        
+        $rowsExistPlayer = $accessor->FromTable('PlayerHolder')->WhereIn('PlayerID', $playerIDs)->FetchStyleAssoc()->FetchAll();
+        $existPlayerIDs = array_column($rowsExistPlayer, 'PlayerID');
+        $notExistPlayerIDs = array_column($playerIDs, $existPlayerIDs);
+        
+        foreach($notExistPlayerIDs as $playerIDs){
+            
+            $metadata = $this->QueryMetadata($metadataURLs[$playerID]);
+            
+            if($metadata === false)
+                continue;
+            
+            $this->CreatePlayer($playerID, $metadata);
+        }
     }
 }
