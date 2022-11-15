73b1abe98efe5b9ab4ae541c522365105ad435f8
alan-Chang-7senses <108779046+alan-Chang-7senses@users.noreply.github.com>
2022-11-15 12:47:38 +0800

    修改RaceHP資料表UpdateTime型別。

--- deploy/RaceHP.sql
+++ deploy/RaceHP.sql
@@ -3,6 +3,6 @@ USE `koa_main`;
 CREATE TABLE IF NOT EXISTS `RaceHP` (
   `RacePlayerID` int(11) NOT NULL DEFAULT 0,
   `HValue` float(10,2) NOT NULL DEFAULT 0.00,
-  `UpdateTime` int(11) NOT NULL DEFAULT 0,
+  `UpdateTime` decimal(20,6) NOT NULL DEFAULT 0.000000,
   PRIMARY KEY (`RacePlayerID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='紀錄比賽每次更新之H值';
