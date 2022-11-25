661937a920aab79f652d4a2149703a7c209fff8f
mingoliu7senses <mingoliu@7senses.com>
2022-11-25 12:30:48 +0800

    更新 SQL : 異動 Koa_static 資料庫 > Announcement 資料表 > CreateTime 欄位說明

--- deploy/AnnouncementTimeZone.sql
+++ deploy/AnnouncementTimeZone.sql
@@ -1,7 +1,7 @@
 use Koa_static;
 
 ALTER TABLE `Announcement`
-	ADD COLUMN `CreateTime` INT(11) NOT NULL DEFAULT '0' COMMENT '創建時間，非發佈時間' AFTER `Content`,
+	ADD COLUMN `CreateTime` INT(11) NOT NULL DEFAULT '0' COMMENT 'UI 顯示公告創建時間' AFTER `Content`,
 	DROP COLUMN `CreateTime`;
 
 UPDATE `Announcement` SET `CreateTime`=1666597667 WHERE `Serial`=1;
