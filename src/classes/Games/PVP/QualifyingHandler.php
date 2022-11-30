ea848192584dbfff1620cc83d40595e05634db16
mingoliu7senses <mingoliu@7senses.com>
2022-11-30 14:30:19 +0800

    更新 API : 偵測賽季讀表流程，Pool (QualifyingData)

--- src/classes/Games/PVP/QualifyingHandler.php
+++ src/classes/Games/PVP/QualifyingHandler.php
@@ -5,13 +5,12 @@ namespace Games\PVP;
 use Consts\EnvVar;
 use Consts\ErrorCode;
 use Consts\Globals;
-use DateTime;
 use Exception;
 use Games\Accessors\QualifyingSeasonAccessor;
 use Games\Consts\RaceValue;
 use Games\Exceptions\RaceException;
 use Games\Pools\ItemInfoPool;
-use Games\Pools\QualifyingSeasonPool;
+use Games\Pools\QualifyingDataPool;
 use Games\Pools\TicketInfoPool;
 use Games\PVP\Holders\TicketInfoHolder;
 use Games\Races\RaceUtility;
@@ -28,37 +27,34 @@ class QualifyingHandler {
     public const MatchLobbies = [RaceValue::LobbyCoinA, RaceValue::LobbyCoinB,
         RaceValue::LobbyPetaTokenA, RaceValue::LobbyPetaTokenB];
 
-    public int $NowSeasonID;
     public array $NowSeasonIDArr;
-
-    private QualifyingSeasonPool $pool;
-    private TicketInfoPool $ticketPool;
-    private QualifyingSeasonAccessor $pdoAccessor;
-    public stdClass $info;
     public array $infoArr;
 
+    private QualifyingDataPool $qualifyingDataPool;
+    private TicketInfoPool $ticketPool;
+    private QualifyingSeasonAccessor $pdoAccessor;
+
     public function __construct() {
-        $this->pool = QualifyingSeasonPool::Instance();
+        $this->qualifyingDataPool = QualifyingDataPool::Instance();
         $this->ticketPool = TicketInfoPool::Instance();
         $this->pdoAccessor = new QualifyingSeasonAccessor();
-        $this->DetectCurrent();
+        $this->DetectQualifying();
     }
 
-    private function DetectCurrent() {
-
-        // 從 Pool 中取得 QualifyingSeasonData 資料表
-        $result = $this->pool-> {"Current"};
-
+    private function DetectQualifying() {
         $this->NowSeasonIDArr = [];
         $this->infoArr = [];
 
-        if ($result != false) {
-            // 有資料
-            foreach ($result as $data) {
-                // 紀錄目前各大廳開放的賽季編號
+        foreach (QualifyingHandler::Lobbies as $lobby) {
+
+            // 輪巡大廳編號 查詢 QualifyingSeasonData 開放賽季資料
+            $data = $this->pdoAccessor->GetOpenQualifyingSeasonData($lobby);
+
+            if ($data !== false) {
+                // 紀錄大廳開放賽季編號
                 $this->NowSeasonIDArr[] = $data->SeasonID;
 
-                // 賽季資料整理至陣列
+                // 賽季資料 整理至陣列
                 $info = new stdClass;
                 $info->ID = $data->ID;
                 $info->SeasonID = $data->SeasonID;
@@ -66,32 +62,29 @@ class QualifyingHandler {
                 $info->Status = $data->Status;
                 $info->Assign = $data->Assign;
                 $info->UpdateTime = $data->UpdateTime;
-                $this->infoArr[] = $info;
+                $this->infoArr[$data->Lobby] = $info;
             }
-        }
-        else {
-            // 無資料
-            $this->NowSeasonIDArr = [];
-            $this->infoArr = [];
         }
     }
     
     public function DetectSeason() {
-
-        // 從 koa_static (QualifyingData) 取得企劃定義的賽季大廳資料
-        $qualifyingData = $this->pdoAccessor->GetQualifyingData();
+        $qualifyingData = [];
         $defSeasonIDArr = [];
-        foreach ($qualifyingData as $data) {
-            $defSeasonIDArr[] = $data->SeasonID;
+        
+        // (QualifyingData) : 取得企劃定義的賽季大廳資料
+        foreach (QualifyingHandler::Lobbies as $lobby) {
+            $data = $this->qualifyingDataPool->{$lobby};
+            if ($data !== false) {
+                $qualifyingData[$lobby] = $data;
+                $defSeasonIDArr[$lobby] = $data->SeasonID;
+            }
         }
 
-        // 從 koa_main (QualifyingSeasonData) 取得遊戲中所有賽季資料
+        // (QualifyingSeasonData) $this->NowSeasonIDArr : 目前遊戲紀錄 進行中賽季資料
         if (count($this->NowSeasonIDArr) > 0) {
-
-            // Pool 已存在 QualifyingSeasonData 資料
             foreach ($this->NowSeasonIDArr as $nowSeasonID) {
+                // 若逾期，則將 Status 改成 0
                 if (in_array($nowSeasonID, $defSeasonIDArr) == false) {
-                    // 若無在賽季列表中，則將 Status 改成 0
                     $bind = ["Status" => 0];
                     $this->pdoAccessor->ModifyQualifyingSeasonData($nowSeasonID, $bind);
                     array_diff($this->NowSeasonIDArr, array($nowSeasonID));
@@ -99,17 +92,21 @@ class QualifyingHandler {
             }
         }
         
-        // 新資料
+        // (QualifyingSeasonData) 新增資料
         if ($qualifyingData != false) {
             foreach ($qualifyingData as $data) {
+                // 不在 目前遊戲紀錄進行中賽季資料 
                 if (in_array($data->SeasonID, $this->NowSeasonIDArr) == false) {
                     $this->pdoAccessor->AddQualifyingSeasonData($data->SeasonID, $data->Lobby);
                 }
             }
         }
 
-        $this->pool->Delete("Current");
-        $this->DetectCurrent();
+        foreach (QualifyingHandler::Lobbies as $lobby) {
+            $this->qualifyingDataPool->Delete($lobby);
+        }
+
+        $this->DetectQualifying();
     }
 
     public function CheckLobbyID(int $lobby) {
@@ -124,10 +121,17 @@ class QualifyingHandler {
         }
     }
 
-    public function GetSeasonRemaintime(): int {
-        $remainTime = $this->info->EndTime - $GLOBALS[Globals::TIME_BEGIN] - ConfigGenerator::Instance()->PvP_B_StopMatch;
-        if ($remainTime < 0)
+    public function GetSeasonRemaintime(int $lobby): int {
+        $qualifyData = $this->qualifyingDataPool-> {$lobby};
+        if ($qualifyData !== false) {
+            $remainTime = $qualifyData->EndTime - $GLOBALS[Globals::TIME_BEGIN] - ConfigGenerator::Instance()->PvP_B_StopMatch;
+            if ($remainTime < 0)
+                $remainTime = 0;
+        }
+        else {
             $remainTime = 0;
+        }
+        
         return (int) $remainTime;
     }
 
@@ -149,14 +153,11 @@ class QualifyingHandler {
     }
 
     public function GetSceneID(int $lobby, int $defaultSceneID): int {
-        switch ($lobby) {
-            case RaceValue::LobbyCoinA:
-            case RaceValue::LobbyCoinB:
-                return $this->info->CoinScene;
-            case RaceValue::LobbyPetaTokenA:
-            case RaceValue::LobbyPetaTokenB:
-                return $this->info->PTScene;
+        $qualifyData = $this->qualifyingDataPool->{$lobby};
+        if ($qualifyData !== false) {
+            return $qualifyData->Scene;
         }
+        
         return $defaultSceneID;
     }
 
@@ -236,14 +237,9 @@ class QualifyingHandler {
     }
 
     public function GetSeasonIDByLobby(int $lobby): int {
-
-        foreach ($this->infoArr as $data) {
-            if ($data->Lobby == $lobby) {
-                return $data->SeasonID;
-            }
+        if (array_key_exists($lobby, $this->infoArr) == true) {
+            return $this->infoArr[$lobby]->SeasonID;
         }
-
-        return 0;
+        return RaceValue::NOSeasonID;
     }
-
 }
