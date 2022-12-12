eba8ebf0c8dab1bcfc9aa6a177825785972bb294
gene <gene@7senses.com>
2022-12-12 14:53:40 +0800

    領取信件改為複數輸入信件ID

--- src/classes/Processors/Mails/ReceiveMailsRewards.php
+++ src/classes/Processors/Mails/ReceiveMailsRewards.php
@@ -14,43 +14,52 @@ use Helpers\InputHelper;
 use Holders\ResultData;
 use Processors\BaseProcessor;
 
-class ReceiveMailsRewards extends BaseProcessor
-{
+class ReceiveMailsRewards extends BaseProcessor {
 
-    public function Process(): ResultData
-    {
-        $userMailID = InputHelper::post('userMailID');
+    public function Process(): ResultData {
+        $userMailIDs = json_decode(InputHelper::post('userMailIDs'));
         $openStatus = InputHelper::post('openStatus');
         $receiveStatus = InputHelper::post('receiveStatus');
 
-        $userMailsHandler = new MailsHandler();
-        $mailInfo = $userMailsHandler->GetUserMailByuUerMailID($_SESSION[Sessions::UserID], $userMailID);
-        if ($mailInfo == false) {
-            throw new ItemException(ItemException::MailNotExist);
-        }
         if ($openStatus != 0)
             $openStatus = 1;
 
-        $itemsArray = [];
-        if ($receiveStatus == 1) {
-            if ($mailInfo->ReceiveStatus == MailValues::ReceiveStatusDone) {
-                throw new ItemException(ItemException::MailRewardsReceived);
+        $userID = $_SESSION[Sessions::UserID];
+        $userBagHandler = new UserBagHandler($userID);
+        $userMailsHandler = new MailsHandler();
+        $totalItems = [];
+        foreach ($userMailIDs as $userMailID) {
+
+            $mailInfo = $userMailsHandler->GetUserMailByuUerMailID($userID, $userMailID);
+            if ($mailInfo == false) {
+                throw new ItemException(ItemException::MailNotExist);
             }
 
-            $items = $userMailsHandler->GetMailItems($userMailID);
-            $userBagHandler = new UserBagHandler($_SESSION[Sessions::UserID]);
-            if ($userBagHandler->CheckAddStacklimit($items) == false) {
-                throw new ItemException(ItemException::UserItemStacklimitReached);
+            if ($receiveStatus == 1) {
+                if ($mailInfo->ReceiveStatus == MailValues::ReceiveStatusDone) {
+                    throw new ItemException(ItemException::MailRewardsReceived);
+                }
+                $addItems = $userMailsHandler->GetMailItems($userMailID);
+                if ($userBagHandler->CheckAddStacklimit($addItems) == false) {
+                    throw new ItemException(ItemException::UserItemStacklimitReached);
+                }
+                ItemUtility::AccumulateItems($totalItems, $addItems);
+                //$userMailsHandler->ReceiveRewards($userID, $userMailID, $openStatus, MailValues::ReceiveStatusDone);
+            } else {
+                //$receiveStatus = $mailInfo->ReceiveStatus;
+                $userMailsHandler->UpdateOpenStatus($userID, $userMailID, $openStatus);
             }
-            foreach ($items as $item) {
-                $userBagHandler->AddItems($item, ItemValue::CauseMail);
+        }
+
+        $itemsArray = [];
+        if ($receiveStatus == 1) {
+            foreach ($userMailIDs as $userMailID) {
+                $userMailsHandler->ReceiveRewards($userID, $userMailID, $openStatus, MailValues::ReceiveStatusDone);
+            }           
+            $userBagHandler->AddItems($totalItems, ItemValue::CauseMail);            
+            foreach ($totalItems as $item) {
                 $itemsArray[] = ItemUtility::GetClientSimpleInfo($item->ItemID, $item->Amount);
             }
-            $userMailsHandler->ReceiveRewards($_SESSION[Sessions::UserID], $userMailID, $openStatus, MailValues::ReceiveStatusDone);
-        }
-        else {
-            $receiveStatus = $mailInfo->ReceiveStatus;
-            $userMailsHandler->UpdateOpenStatus($_SESSION[Sessions::UserID], $userMailID, $openStatus);
         }
 
         $result = new ResultData(ErrorCode::Success);
@@ -59,6 +68,5 @@ class ReceiveMailsRewards extends BaseProcessor
         $result->rewardItems = $itemsArray;
         return $result;
     }
-
 
 }
