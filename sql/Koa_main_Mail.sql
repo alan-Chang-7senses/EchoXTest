652f405ccc004d8551b2ea51c2663ccccae501e2
gene <gene@7senses.com>
2022-12-14 17:35:44 +0800

    koa_main_Mail.sql 修改內文

--- sql/Koa_main_Mail.sql
+++ sql/Koa_main_Mail.sql
@@ -1,33 +1,5 @@
--- --------------------------------------------------------
--- 主機:                           127.0.0.1
--- 伺服器版本:                        10.9.3-MariaDB-1:10.9.3+maria~ubu2204 - mariadb.org binary distribution
--- 伺服器作業系統:                      debian-linux-gnu
--- HeidiSQL 版本:                  11.3.0.6295
--- --------------------------------------------------------
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
--- 傾印 koa_main 的資料庫結構
-CREATE DATABASE IF NOT EXISTS `koa_main` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
-USE `koa_main`;
-
--- 傾印  資料表 koa_main.Configs 結構
-CREATE TABLE IF NOT EXISTS `Configs` (
-  `Name` varchar(255) NOT NULL,
-  `Value` varchar(255) NOT NULL,
-  `Comment` varchar(255) NOT NULL,
-  PRIMARY KEY (`Name`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COMMENT='雜項設置';
-
--- 正在傾印表格  koa_main.Configs 的資料：~42 rows (近似值)
-/*!40000 ALTER TABLE `Configs` DISABLE KEYS */;
-INSERT INTO `Configs` (`Name`, `Value`, `Comment`) VALUES
+REPLACE INTO `Configs` (`Name`, `Value`, `Comment`) VALUES
 	('AllPlayerLevel', '100', '強制指定所有角色等級(0=無效)'),
 	('AllSkillLevel', '5', '強制指定所有角色技能等級(0=無效)'),
 	('AmountRacePlayerMax', '8', '開房最大人數'),
@@ -71,9 +43,8 @@ INSERT INTO `Configs` (`Name`, `Value`, `Comment`) VALUES
 	('TimelimitRaceFinish', '300', '競賽完賽時限(秒)'),
 	('TutorialRewards', '[{"Step":1, "ItemID":1003, "Amount":1}]', '新手引導獎勵'),
 	('TutorialSceneID', '4000', '新手引導賽場編號');
-/*!40000 ALTER TABLE `Configs` ENABLE KEYS */;
 
--- 傾印  資料表 koa_main.UserMails 結構
+DROP TABLE IF EXISTS `UserMails`;
 CREATE TABLE IF NOT EXISTS `UserMails` (
   `UserMailID` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '信件流水號',
   `UserID` int(10) NOT NULL COMMENT '玩家ID',
@@ -91,12 +62,3 @@ CREATE TABLE IF NOT EXISTS `UserMails` (
   KEY `OpenStatus` (`OpenStatus`),
   KEY `FinishTime` (`FinishTime`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- 正在傾印表格  koa_main.UserMails 的資料：~0 rows (近似值)
-/*!40000 ALTER TABLE `UserMails` DISABLE KEYS */;
-/*!40000 ALTER TABLE `UserMails` ENABLE KEYS */;
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
