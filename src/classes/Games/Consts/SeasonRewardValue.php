2cecc551edf3c1a03ea3b9d115cd28aef131f723
sigma-lin-7senses <101098088+sigma-7senses@users.noreply.github.com>
2023-01-05 10:29:44 +0800

    優化處理

---
+++ src/classes/Games/Consts/SeasonRewardValue.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Games\Consts;
+
+/**
+ * Description of SeasonRewardValue
+ *
+ * @author Lian Zhi Wei <zhiwei.lian@7senses.com>
+ */
+class SeasonRewardValue {
+    
+    const NotHandle = 0;
+    const Success = 1;
+    const RewardDataFailure = 2;
+    const NoRankingData = 3;
+    const MailIDFailure = 4;
+}
