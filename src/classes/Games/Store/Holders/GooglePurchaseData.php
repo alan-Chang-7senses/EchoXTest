c2b96f1842169b3bb480e6cfea05618beccd2c9c
gene <gene@7senses.com>
2023-01-19 17:30:41 +0800

    移除多於文字

--- src/classes/Games/Store/Holders/GooglePurchaseData.php
+++ src/classes/Games/Store/Holders/GooglePurchaseData.php
@@ -5,6 +5,7 @@ namespace Games\Store\Holders;
 use Games\Exceptions\StoreException;
 use Games\Store\GoogleUtility;
 use stdClass;
+use function GuzzleHttp\json_decode;
 
 /*
  * Description of GoogleHolder
@@ -27,7 +28,6 @@ class GooglePurchaseData extends stdClass {
         $this->productId = $jsonData->productId;
         $this->purchaseTime = $jsonData->purchaseTime;
         $this->purchaseState = $jsonData->purchaseState;
-        //$this->developerPayload = $jsonData->developerPayload;
         $this->purchaseToken = $jsonData->purchaseToken;
 
         $metadata = json_decode($metatstr);
@@ -38,11 +38,22 @@ class GooglePurchaseData extends stdClass {
         $this->localizedPrice = $metadata->localizedPrice;
     }
 
+    /** @var string 當地價格字串 */
     public string $localizedPriceString;
+
+    /** @var string 當地標題 */
     public string $localizedTitle;
+
+    /** @var string 當地描述 */
     public string $localizedDescription;
+
+    /** @var string 當地幣別 */
     public string $isoCurrencyCode;
+
+    /** @var int 當地價格 */
     public float $localizedPrice;
+
+    /** @var int 交易序號 */
     public string $transactionID;
 
     /** @var int 交易的專屬訂購單。 */
@@ -59,9 +70,6 @@ class GooglePurchaseData extends stdClass {
 
     /** @var string 訂購單的購買狀態 */
     public string $purchaseState;
-
-    /** @var string 開發人員指定的字串 */
-    //public string $developerPayload;
 
     /** 專門用來識別指定商品和使用者成對資料的購買交易權杖 */
     public string $purchaseToken;
